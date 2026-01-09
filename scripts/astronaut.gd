a7e2b4d4100223e8609694568b4d8d3d25f66f94
Shreya <153142441+shreya-0718@users.noreply.github.com>
2026-01-08 22:21:34 -0600

    why is camera so shaky

--- scripts/astronaut.gd
+++ scripts/astronaut.gd
@@ -1,13 +1,11 @@
 extends CharacterBody2D
 
-@export var move_speed := 200.0
-@export var jump_force := 350.0
+@export var move_speed = 200.0
+@export var jump_force = 350.0
 @export var glide_gravity_scale := 0.25 
 
 func _physics_process(delta):
-	print(Input.is_action_pressed("left"), Input.is_action_pressed("right"))
-	
-	var direction := 0.0
+	var direction = 0.0
 	if Input.is_action_pressed("left"):
 		direction -= 1
 	if Input.is_action_pressed("right"):
@@ -19,7 +17,7 @@ func _physics_process(delta):
 
 	if Input.is_action_pressed("glide") and not is_on_floor() and velocity.y > 0:
 		velocity.y += gravity * glide_gravity_scale * delta
-	else:
+	elif not is_on_floor():
 		velocity.y += gravity * delta
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
