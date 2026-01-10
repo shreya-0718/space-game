277b4ff01c868cbcf227ef4444304eb64b79f9ce
avani-dracula <avanijadr@gmail.com>
2026-01-09 23:38:05 -0600

    fixed a problem with scrolling level page

--- scripts/levelpage.gd
+++ scripts/levelpage.gd
@@ -1,5 +1,16 @@
 extends Node2D
 
+@onready var stars = [$star_container/star1, $star_container/star2, $star_container/star3]
+
+func _ready():
+	update_stars()
+
+func update_stars():
+	for i in range(stars.size()):
+		stars[i].modulate = Color(1, 1, 1, 1) if i < global.mercury_stars else Color (1, 1, 1, 0.3)
+
+
+
 #ARROW KEYS
 var scroll_speed := 400
 
