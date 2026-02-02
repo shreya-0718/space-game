95a00b0d9bdbbfe006fc957f07b60964b7449003
Shreya <153142441+shreya-0718@users.noreply.github.com>
2026-02-02 00:00:22 -0600

    earth forest finally fixed!!!

--- scripts/earth_forest.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-@onready var quicksand1 = $Node2D/AnimatedSprite2D
-@onready var quicksand2 = $Node2D/AnimatedSprite2D2
-var started := false
-
-func _ready():
-	$Node2D/AnimationPlayer.play("sync")
