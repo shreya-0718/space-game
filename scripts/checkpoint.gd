df049c78e86f00c6fa181ffe11ffb2edb4e6dc85
avani-dracula <avanijadr@gmail.com>
2026-02-01 23:49:53 -0600

    everything update

--- scripts/checkpoint.gd
+++ scripts/checkpoint.gd
@@ -1,1 +1,13 @@
 extends Area2D
+
+@onready var white = get_node("white")
+@onready var purple = get_node("purple")
+
+func _ready():
+	purple.visible = false
+	
+func _on_body_entered(body):
+	if body.name == "astronaut":
+		global.passedcheckpoint = true
+		purple.visible = true
+		white.visible = false
