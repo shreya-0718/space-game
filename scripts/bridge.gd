318dd491107ba0740eefb2b74558e11ed301d905
avani-dracula <avanijadr@gmail.com>
2026-02-02 16:44:36 -0600

    earth update

---
+++ scripts/bridge.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+@onready var bridgepoint = get_node("bridgepoint")
+
+func _on_body_entered(body):
+	if body.name == "astronaut":
+		body.global_position = bridgepoint.global_position
