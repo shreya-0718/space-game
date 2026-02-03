eca405604436b7bb4b9d038323e72f3cc1acc69d
Shreya <153142441+shreya-0718@users.noreply.github.com>
2026-02-02 21:04:51 -0600

    fixed story button

--- scripts/texture_button.gd
+++ scripts/texture_button.gd
@@ -1,6 +1,5 @@
 extends TextureButton
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,6 +9,6 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
-func _on_pressed() -> void:
+func _on_pressed():
+	print("WORK PLEASEE")
 	get_tree().change_scene_to_file("res://scenes/levelpage1.tscn")
