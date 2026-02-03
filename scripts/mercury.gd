6efe16c61195b37e7f3d40808bbe7315c02b310e
avani-dracula <avanijadr@gmail.com>
2026-02-02 23:58:47 -0600

    loading?

--- scripts/mercury.gd
+++ scripts/mercury.gd
@@ -1,6 +1,8 @@
 extends TextureButton
 
+@onready var txtt = get_node("../Label2")
 
 func _on_pressed():
+	txtt.visible = true 
 	global.mercury_stars = 0
 	get_tree().change_scene_to_file("res://scenes/mercury.tscn")
