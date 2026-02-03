c106b4e4c50334228737339b5d1e33ebd6c6186a
Shreya <153142441+shreya-0718@users.noreply.github.com>
2026-02-02 19:39:23 -0600

    fixed venus progress bar

--- scripts/earthbutton.gd
+++ scripts/earthbutton.gd
@@ -3,4 +3,4 @@ extends TextureButton
 
 func _on_pressed():
 	global.earth_stars = 0
-	#get_tree().change_scene_to_file("res://scenes/earth_mountains.tscn")
+	get_tree().change_scene_to_file("res://scenes/earth_mountains.tscn")
