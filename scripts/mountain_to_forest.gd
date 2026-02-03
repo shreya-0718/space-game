fd8d84ab72c4a57525904c1076f46b67974a0f6c
Shreya <153142441+shreya-0718@users.noreply.github.com>
2026-02-02 20:40:29 -0600

    mountains and back buttons

---
+++ scripts/mountain_to_forest.gd
@@ -0,0 +1,12 @@
+extends Area2D
+@onready var text = get_node("Label")
+
+var player_inside = false
+
+func _on_body_entered(body):
+	player_inside = true
+	text.visible = true
+	
+func _process(delta):
+	if player_inside and Input.is_action_just_pressed("space"):
+		get_tree().change_scene_to_file("res://scenes/earth_forest.tscn")
